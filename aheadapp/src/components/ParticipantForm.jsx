a88ab81ea61e69195b777f07c55069ab49dca536
Massit Clement <clement.massit@u-bordeaux.fr>
2024-12-03 11:30:32 +0100

    remove secret

--- aheadapp/src/components/ParticipantForm.jsx
+++ aheadapp/src/components/ParticipantForm.jsx
@@ -129,25 +129,6 @@ const ParticipantForm = ({ participants, setParticipants }) => {
       }
       // using Twilio SendGrid's v3 Node.js Library
       // https://github.com/sendgrid/sendgrid-nodejs
-
-      // sgMail.setApiKey(
-      //   "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"
-      // );
-      // const msg = {
-      //   to: "test@example.com", // Change to your recipient
-      //   from: "test@example.com", // Change to your verified sender
-      //   subject: "Sending with SendGrid is Fun",
-      //   text: "and easy to do anywhere, even with Node.js",
-      //   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-      // };
-      // sgMail
-      //   .send(msg)
-      //   .then(() => {
-      //     console.log("Email sent");
-      //   })
-      //   .catch((error) => {
-      //     console.error(error);
-      //   });
     });
   };
 
